7113483fe44d92804e792ee08cedc75dba2832c8
hamza <hamzash863@gmail.com>
2022-04-24 18:15:23 +0300

    Code Refactoring

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// define Cards Icons
 const card = [
   "fa-diamond",
   "fa-diamond",
@@ -16,48 +17,51 @@ const card = [
   "fa-bomb",
   "fa-bomb",
 ];
-let openCard = [];
-let openedCard = [];
 
+// define Dom elements
 const movesCounter = document.querySelector(".moves");
 const restart = document.querySelector(".restart");
 let timerWidget = document.querySelector(".timer");
 let stars = document.querySelectorAll(".stars");
 
+// define game variables
 let moves = 0;
-let cardMatched = 7;
+let cardMatched = 0;
 let firstClick = false;
 let interval;
 let sec = 0;
 let min = 0;
+let openCard = [];
+let openedCard = [];
 
+// create one card with here data
 function generateCard(card) {
   return `<li class="card" data-card=${card}><i class="fa ${card}"></i></li>`;
 }
 
+// shuffle function from stackoverflow
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/
 function shuffle(array) {
-  var currentIndex = array.length,
-    temporaryValue,
+  let currentIndex = array.length,
     randomIndex;
 
-  while (currentIndex !== 0) {
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
 
   return array;
 }
 
-function initGame() {
-  const deck = document.querySelector(".deck");
-  cardTemplate = shuffle(card).map((card) => generateCard(card));
-  movesCounter.innerText = moves;
-  deck.innerHTML = cardTemplate.join("");
-}
-
+// define game timer
 function timer() {
   interval = setInterval(function () {
     timerWidget.innerHTML = `0:${min}:${sec}`;
@@ -68,6 +72,7 @@ function timer() {
     }
   }, 1000);
 
+  // restart the game and play again
   restart.addEventListener("click", function (e) {
     stars.forEach((star) => star.classList.remove("fill-star"));
     clearInterval(interval);
@@ -84,34 +89,48 @@ function timer() {
   });
 }
 
+// Preparing the game to play
+function initGame() {
+  const deck = document.querySelector(".deck");
+  cardTemplate = shuffle(card).map((card) => generateCard(card));
+  movesCounter.innerText = moves;
+  deck.innerHTML = cardTemplate.join("");
+}
 initGame();
 
+// draw the cards on browser window
 const cards = document.querySelectorAll(".card");
 cards.forEach((card) => {
+  // click card event
   card.addEventListener("click", function (e) {
+    // check if the game has started
     if (firstClick == false) {
       timer();
       firstClick = true;
     }
+
+    // check from card status
     if (
       !card.classList.contains("open") &&
       !card.classList.contains("show") &&
       !card.classList.contains("match")
     ) {
       openCard.push(card);
+
+      //open just 2 cards
       if (openCard.length < 3) {
         card.classList.add("open", "show");
 
         if (openCard.length == 2) {
+          // open the Similar cards
           if (openCard[0].dataset.card == openCard[1].dataset.card) {
             cardMatched += 1;
             openCard.forEach((element) => {
               element.classList.add("open", "show", "match");
             });
-            openedCard.push(openCard[0]);
-            openedCard.push(openCard[1]);
             openCard = [];
           } else {
+            // close the difference card
             setTimeout(() => {
               openCard.forEach((element) => {
                 element.classList.remove("open", "show");
@@ -119,24 +138,32 @@ cards.forEach((card) => {
               openCard = [];
             }, 500);
           }
+
+          // refresh counter moves
           moves++;
           movesCounter.innerText = moves;
         }
       }
     }
-    if (openedCard.length >= 16) {
+    // if game has finished
+    if (cardMatched >= 8) {
+      // stop game and timer
+      clearInterval(interval);
+      firstClick = false;
+
+      // draw winner stars
       if (min == 0 && sec <= 30) {
-        stars[0].classList.add("fill-star");
-        stars[1].classList.add("fill-star");
-        stars[2].classList.add("fill-star");
+        stars.forEach(function (e) {
+          e.classList.add("fill-star");
+        });
       } else if (min <= 1) {
         stars[0].classList.add("fill-star");
         stars[1].classList.add("fill-star");
       } else {
         stars[0].classList.add("fill-star");
       }
-      clearInterval(interval);
-      firstClick = false;
+
+      // winner message
       const message = `Booooooyyyyyyaaaaaa, The monster end the game with ${moves} moves in time ${min}:${sec}`;
       window.alert(message);
     }
